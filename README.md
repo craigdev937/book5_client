adbfb5228e32bef402e11a48294721394d03116f
Craig <indigomx9@gmail.com>
2022-01-13 14:13:25 -0500

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,7 @@
+# MERN Project
+
+Initial setup of the project created with React, React Router 6, Redux Toolkit, and TypeScript.
+
+
+
+
---
+++ src/app/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+export const App = (): JSX.Element => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, quasi omnis! A dolore facere illo enim tempora molestias atque corporis impedit pariatur amet? Totam ipsa reprehenderit debitis consequuntur modi facere!</p>
+        </React.Fragment>
+    );
+};
+
+
+
+
